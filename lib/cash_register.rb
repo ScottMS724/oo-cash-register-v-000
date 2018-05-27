8b1c3522b594a2afce503eda17bb4a5d9b2c54f6
Scott Silverman <scottms724@gmail.com>
2018-05-27 19:27:21 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
   
   def add_item(title, price, amount_bought = 1)
     @price = price
-    @@items << title * amount_bought
+    @@items << title 
     @total = @total + price * amount_bought
   end 
   
